2694d1c15cc7e66f163b4dcdf8d09a64ded31599
Thomas Mandelz <tmandelz@hotmail.com>
2023-04-16 12:36:22 +0200

    add convnext tiny models

--- modelling/convnext.py
+++ modelling/convnext.py
@@ -1,3 +1,15 @@
+#%% [markdown]
+"""
+Sources:
+https://github.com/facebookresearch/ConvNeXt
+
+@Article{liu2022convnet,
+  author  = {Zhuang Liu and Hanzi Mao and Chao-Yuan Wu and Christoph Feichtenhofer and Trevor Darrell and Saining Xie},
+  title   = {A ConvNet for the 2020s},
+  journal = {Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)},
+  year    = {2022},
+}
+"""
 #%%
 import os
 print(os.getcwd())
@@ -5,7 +17,8 @@ if os.getcwd().endswith("modelling"):
     os.chdir("..")
 
 #%%
-is_cscs_run = False
+is_cscs_run = True
+
 if is_cscs_run:
     os.chdir("ccv1/tierli_ahluege/")
     print(os.getcwd())
@@ -21,22 +34,131 @@ from torchvision import transforms,models
 # wandb Notebook Setup
 %env "WANDB_NOTEBOOK_NAME" "ccv1_convnext"
 %env WANDB_SILENT=True
+
+#%%
+
+def convnext_():
+    model = models.convnext_tiny(weights=True,)
+    model.classifier[2] =nn.Linear(in_features=768, out_features=8, bias=True)
+   
+    return model
+
+#%%
+model_name = "convnext_tiny"
+pretrained_model = "convnext"
+convnext_transformer = CCV1Transformer(
+    transforms.Compose([None_Transform()]), "model_specific", pretrained_model
+).getCompose(True)
+
+#%%
+convnext_().classifier
+#%%
+convnext_transformer
+# %%
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_,)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=False,test_model=False,batchsize_train_data=128,lr = 3e-4)
+
+# %%
+convnext.submission(model_name)
+
+
 #%%
 def convnext_():
-    model = models.convnext_base(weights=True)
-    model.classifier=model.fc = nn.Sequential(
-            nn.Linear(2048, 100),  # dense layer takes a 2048-dim input and outputs 100-dim
-            nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
-            nn.Dropout(0.1),  # common technique to mitigate overfitting
-            nn.Linear(
-                100, 8
-            ),  # final dense layer outputs 8-dim corresponding to our target classes
-        )
+    model = models.convnext_tiny(weights=True,)
+    model.classifier[2] =nn.Dropout(0.2)
+    model.classifier.add_module("3",nn.Linear(in_features=768, out_features=8, bias=True))
+   
     return model
+
+convnext_().classifier
+
+
+#%%
+model_name = "convnext_tiny_dropout_0.2"
+pretrained_model = "convnext"
 convnext_transformer = CCV1Transformer(
-    transforms.Compose([None_Transform()]), "model_specific", "resnet"
-).getCompose()
+    transforms.Compose([None_Transform()]), "model_specific", pretrained_model
+).getCompose(True)
+
+#%%
+convnext_().classifier
+#%%
+convnext_transformer
+# %%
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_,)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=False,test_model=False,batchsize_train_data=128,lr = 3e-4)
+
+# %%
+convnext.submission(model_name)
+
+#%%
+def convnext_small():
+    model = models.convnext_small(weights=True,)
+    model.classifier[2] =nn.Linear(in_features=768, out_features=8, bias=True)
+    return model
+
+convnext_small().classifier
+
+
+#%%
+model_name = "convnext_small"
+pretrained_model = "convnext"
+convnext_transformer = CCV1Transformer(
+    transforms.Compose([None_Transform()]), "model_specific", pretrained_model
+).getCompose(True)
+
+#%%
+convnext_transformer
+# %%
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_small,)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=False,test_model=False,batchsize_train_data=32,lr = 3e-4)
+
+# %%
+convnext.submission(model_name)
+
+
+
+
+
+
+#%%
+def convnext_():
+    model = models.convnext_tiny(weights=True,)
+    model.classifier[2] =nn.Linear(in_features=768, out_features=124, bias=True)
+    model.classifier.add_module("3",nn.ReLU(inplace=True))
+    model.classifier.add_module("4",nn.Dropout(0.5))
+    model.classifier.add_module("5",nn.Linear(in_features=124, out_features=8, bias=True))
+   
+    return model
+
+#%%
+model_name = "convnext_tiny_augmix_dropout_0.5"
+pretrained_model = "convnext"
+#%%
+convnext_transformer = CCV1Transformer(
+    transforms.Compose([transforms.AugMix()]), "model_specific", pretrained_model
+).getCompose(True)
+
 # %%
 convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_)
-convnext.train_model("convnext without augmentation", "convnext", num_epochs=6, test_model=False,batchsize_train_data=128)
+convnext.train_model(model_name, pretrained_model, num_epochs=8, cross_validation=False,test_model=False,batchsize_train_data=128)
+
+# %%
+convnext.submission(model_name)
+
+#%%
+model_name = "convnext_tiny_horizontalflip_dropout_0.5"
+pretrained_model = "convnext"
+#%%
+convnext_transformer = CCV1Transformer(
+    transforms.Compose([transforms.RandomHorizontalFlip()]), "model_specific", pretrained_model
+).getCompose(True)
+convnext_transformer
+
+# %%
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_)
+convnext.train_model(model_name, pretrained_model, num_epochs=8, cross_validation=False,test_model=False,batchsize_train_data=128)
+
+# %%
+convnext.submission(model_name)
 # %%
