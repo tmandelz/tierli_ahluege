3f74fcf27019391dbac013beb344a6a6ad55b85a
jan.zwicky <jan.zwicky@students.fhnw.ch>
2023-05-03 23:51:26 +0200

    add freeze models

--- modelling/convnext_unfreeze.py
+++ modelling/convnext_unfreeze.py
@@ -6,7 +6,7 @@ if os.getcwd().endswith("modelling"):
     os.chdir("..")
 
 #%%
-is_cscs_run = True
+is_cscs_run = False
 
 if is_cscs_run:
     os.chdir("ccv1/tierli_ahluege/")
@@ -31,21 +31,16 @@ def convnext_tiny_unfreeze():
     model = models.convnext_tiny(weights=True)
     # Disable gradients on all model parameters to freeze the weights
     for param in model.parameters():
-        param.requires_grad = True
-    model.classifier[2] =nn.Linear(in_features=768, out_features=8, bias=True)
+        param.requires_grad = False
+    model.classifier[2] =nn.Sequential(
+        nn.Dropout(p=0.5),
+    nn.Linear(in_features=768, out_features=128, bias=True),
+    nn.Linear(in_features=128, out_features=8, bias=True))
     return model
-#%%
-
-# model = convnext_tiny_unfreeze()
-# for child in model.children():
-#     for param in child.parameters():
-#         param.requires_grad = True
-
-
 
 # [i.shape for i in convnext_tiny_unfreeze().parameters()]
 #%%
-model_name = "convnext_tiny_unfreeze_weights"
+model_name = "convnext_tiny_freeze_weights_dropout"
 pretrained_model = "convnext"
 convnext_transformer = CCV1Transformer(
     transforms.Compose([None_Transform()]), "model_specific", pretrained_model
@@ -53,9 +48,44 @@ convnext_transformer = CCV1Transformer(
 
 # %%
 convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_tiny_unfreeze,)
-convnext.train_model(model_name, pretrained_model, num_epochs=1, cross_validation=False, test_model=True, batchsize_train_data=128, lr = 3e-4)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=True, test_model=False, batchsize_train_data=128, lr = 3e-4,num_workers=0)
 
 # %%
 convnext.submission(model_name)
+# %%
+def convnext_small_unfreeze():
+    model = models.convnext_small(weights=True)
+    # Disable gradients on all model parameters to freeze the weights
+    for param in model.parameters():
+        param.requires_grad = False
+    model.classifier[2] =nn.Sequential(
+        nn.Dropout(p=0.5),
+    nn.Linear(in_features=768, out_features=128, bias=True),
+    nn.Linear(in_features=128, out_features=8, bias=True))
+    return model
+# %%
+model_name = "convnext_small_freeze_weights_dropout"
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_small_unfreeze,)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=True, test_model=False, batchsize_train_data=128, lr = 3e-4,num_workers=0)
+# %%
 
+    
+def convnext_small_unfreeze_less():
+    model = models.convnext_small(weights=True)
+    # Disable gradients on all model parameters to freeze the weights
+    n = len(list(model.parameters()))
+    for param in model.parameters():
+        n-=1
+        if n < 21:
+            break
+        param.requires_grad = False
+    model.classifier[2] =nn.Sequential(
+        nn.Dropout(p=0.5),
+    nn.Linear(in_features=768, out_features=8, bias=True))
+    return model
+
+# %%
+model_name = "convnext_small_freeze_less2"
+convnext = CCV1_Trainer(DataModule(convnext_transformer), convnext_small_unfreeze_less,)
+convnext.train_model(model_name, pretrained_model, num_epochs=5, cross_validation=True, test_model=False, batchsize_train_data=128, lr = 3e-4,num_workers=0)
 # %%
